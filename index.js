c9f47642e8604867093515847bb1ca25a221e5b4
Damian <damian9777@gmail.com>
2021-05-14 18:14:27 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+//Toggling the sidebar menu
+document.getElementById("navButton").onclick=function(){
+    let navbar=document.getElementById("navbar")
+    navbar.classList.toggle("hide");
+    navbar.classList.toggle("show");
+};
