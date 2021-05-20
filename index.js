1e367c07a83c5ef21efa0f8fb495dc068a9ce41c
Damian <damian9777@gmail.com>
2021-05-20 11:54:15 +0200

    initial contact info, unfolding about me dummy text

--- index.js
+++ index.js
@@ -26,15 +26,15 @@ navButton.onclick=function(){
     let navbar=document.getElementById('navbar');
     navbar.classList.toggle('hide');
     navbar.classList.toggle('show');
-    let showAboutMe=document.getElementById('showAboutMe').classList;
-    showAboutMe.toggle('hide');
-    showAboutMe.toggle('show');
 };
 
 //toggle About Me section
 document.getElementById('toggleAboutMe').onclick=function(){
     document.getElementById('showAboutMe').classList.toggle('disabled');
     document.getElementById('lessAboutMe').classList.toggle('disabled');
+    let moreAboutMe=document.getElementById('moreAboutMe');
+    moreAboutMe.classList.toggle('hide');
+    moreAboutMe.classList.toggle('show');
 }
 
 //on menu click
